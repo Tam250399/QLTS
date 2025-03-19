923f10b7f0c02f71a65ba74bc6c817e518961ee7
Hoàng Lương Văn <hoangsolution99@gmail.com>
2025-03-19 09:59:02 +0700

    Thêm bảng TS_TAI_SAN_LICH_SU

---
+++ BackEnd/Libraries/GS.Core/Domain/TaiSans/TaiSanLichSu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GS.Core.Domain.TaiSans
+{
+    public partial class TaiSanLichSu : BaseEntity
+    {
+        Decimal TAI_SAN_ID { get; set; }
+        decimal? NGUOI_TAO_ID { get; set; }
+        string HOAT_DONG { get; set; }
+        public DateTime? NGAY_TAO { get; set; }
+    }
+}
