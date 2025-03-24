5bf8394ffd50f88eb86518b3c3db4e4b329f9450
nguyentuananh23101996 <45446063+nguyentuananh23101996@users.noreply.github.com>
2025-03-24 11:05:45 +0700

    Fixed .gitignore

--- BackEnd/Presentation/GS.WebApi/obj/Debug/netcoreapp2.2/GS.WebApi.AssemblyInfo.cs
+++ BackEnd/Presentation/GS.WebApi/obj/Debug/netcoreapp2.2/GS.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GS.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eb64140bb9dac0c4168329c1d559fc68e43d9d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b67b675aec8aecde2ded7877a70f2c45ce485c2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GS.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GS.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
